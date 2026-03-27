a8ae2eafc6cfd72485894cee03d1d6ec73fb06a9
Om Kariya <om.kariya@tssconsultancy.com>
2026-03-27 19:25:51 +0530

    added changes

--- target/generated-sources/annotations/com/emiLoan/EMILoan/mapper/StrategyAuditMapperImpl.java
+++ target/generated-sources/annotations/com/emiLoan/EMILoan/mapper/StrategyAuditMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-26T17:44:33+0530",
+    date = "2026-03-27T10:00:31+0530",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.10 (Amazon.com Inc.)"
 )
 @Component
--- target/generated-sources/annotations/com/emiLoan/EMILoan/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/emiLoan/EMILoan/mapper/UserMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-03-26T17:44:33+0530",
+    date = "2026-03-27T10:00:32+0530",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.10 (Amazon.com Inc.)"
 )
 @Component
